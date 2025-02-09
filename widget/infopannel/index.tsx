60c59def2f1e7961efa473239dd8c62177b04cc9
caioasmuniz <caiomuniz888@gmail.com>
2025-02-09 17:10:26 -0300

    removed unused code

--- widget/infopannel/index.tsx
+++ widget/infopannel/index.tsx
@@ -5,23 +5,11 @@ import { bind, Variable } from "astal";
 const hyprland = Hyprland.get_default()
 import Media from "./media";
 import Battery from "./battery"
-// import Calendar from "./calendar"
 
-const Title = () => <box
-  halign={Gtk.Align.CENTER} 
-  // css={`
-  // border-radius:12px;
-  // padding:4px;
-  // background: alpha(@theme_bg_color, 0.75);`}
-  >
-  <label
-    label={"Info Pannel"} 
-    // css={`
-    //   font-size:2.5em;
-    //   font-weight:bold;
-    //   color:@theme_text_color`}
-       />
-</box>
+const Title = () =>
+  <box halign={Gtk.Align.CENTER}>
+    <label label={"Info Pannel"} />
+  </box>
 
 export default (vertical: Variable<boolean>) => <window
   name="infopannel"
@@ -31,15 +19,10 @@ export default (vertical: Variable<boolean>) => <window
   margin={12} visible={false}
   anchor={bind(vertical).as(vertical => vertical ?
     Astal.WindowAnchor.LEFT :
-    Astal.WindowAnchor.TOP
-  )}
-  monitor={bind(hyprland, "focusedMonitor").as(m => m.id)}
-  // css={`border-radius:12px;
-  // background: alpha(@theme_bg_color, 0.25);`}
- >
-  <box vertical spacing={8}
-    // css={`padding: 4px`}
-    >
+    Astal.WindowAnchor.TOP)}
+  monitor={bind(hyprland, "focusedMonitor")
+    .as(m => m.id)}>
+  <box vertical spacing={8}>
     <box spacing={8}>
       <Gtk.Calendar />
       <box vertical spacing={8}>
