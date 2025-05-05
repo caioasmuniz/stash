0eb0c6aa551b03a387c8bd4beb08c6f400871bd6
caioasmuniz <caiomuniz888@gmail.com>
2025-05-05 18:40:42 -0300

    refactors audioconfig to revealer

--- widget/quicksettings/audioConfig.tsx
+++ widget/quicksettings/audioConfig.tsx
@@ -1,31 +1,37 @@
 import Wireplumber from "gi://AstalWp"
-import { bind } from "ags/state"
+import { bind, State } from "ags/state"
 import { For, Gtk } from "ags/gtk4"
 import { Slider, SliderType } from "../common/slider"
 
 const audio = Wireplumber.get_default()!.audio
+const visible = new State<boolean>(false)
 
 export default () =>
   <box
-    orientation={Gtk.Orientation.VERTICAL}
-    spacing={4}>
-    <Slider type={SliderType.AUDIO} />
-    <menubutton>
-      <popover>
-        <box
-          orientation={Gtk.Orientation.VERTICAL}>
-          <For each={bind(audio, "speakers")}>
-            {speaker =>
-              <Gtk.CheckButton
+    cssClasses={["audio-config"]}
+    orientation={Gtk.Orientation.VERTICAL}>
+    <button $clicked={() => visible.set(!visible.get())}>
+      <Slider type={SliderType.AUDIO} />
+    </button>
+    <Gtk.Revealer revealChild={bind(visible)}>
+      <box
+        orientation={Gtk.Orientation.VERTICAL}>
+        <For each={bind(audio, "speakers")}>
+          {speaker =>
+            <Gtk.ToggleButton
+              active={bind(speaker, "isDefault")}
+              $activate={() => {
+                speaker.set_is_default(true);
+              }}>
+              <label
+                _type="label"
                 label={speaker.description}
-                active={bind(speaker, "isDefault")}
-                $={self => self.connect("notify::active", self => {
-                  speaker.isDefault = self.state
-                })}>
-              </Gtk.CheckButton>
-            }
-          </For>
-        </box>
-      </popover>
-    </menubutton>
+                wrap
+                maxWidthChars={10}
+              />
+            </Gtk.ToggleButton>
+          }
+        </For>
+      </box>
+    </Gtk.Revealer >
   </box >
