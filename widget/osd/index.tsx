2c2d50802a496f3c4130251de3d66cf9541c9879
caioasmuniz <caiomuniz888@gmail.com>
2025-05-15 16:18:03 -0300

    simplifies osd addition

--- widget/osd/index.tsx
+++ widget/osd/index.tsx
@@ -1,5 +1,5 @@
 import Wireplumber from "gi://AstalWp"
-import { bind } from "ags/state"
+import { bind, derive } from "ags/state"
 import Brightness from "../../lib/brightness"
 import Slider from "./slider"
 import app from "ags/gtk4/app"
@@ -11,6 +11,22 @@ const brightness = Brightness.get_default()
 const audio = Wireplumber.get_default()!.audio
 const hyprland = AstalHyprland.get_default()
 
+const popupList: Gtk.Revealer[] = [
+  <Popup
+    observable={bind(audio.defaultSpeaker, "volume")}
+    widget={Slider({
+      iconName: bind(audio.defaultSpeaker, "volumeIcon"),
+      binding: bind(brightness, "screen"),
+    })} /> as Gtk.Revealer,
+
+  <Popup
+    observable={bind(brightness, "screen")}
+    widget={Slider({
+      iconName: "display-brightness-symbolic",
+      binding: bind(brightness, "screen"),
+    })} /> as Gtk.Revealer
+];
+
 export default () =>
   <window
     name={`osd`}
@@ -18,24 +34,18 @@ export default () =>
     application={app}
     margin={24}
     layer={Astal.Layer.OVERLAY}
-    monitor={bind(hyprland, "focusedMonitor").as(m => m.id)}
+    monitor={bind(hyprland.focusedMonitor,"id")}
     cssClasses={["osd-popup"]}
     anchor={Astal.WindowAnchor.BOTTOM}
-    visible>
+    visible={bind(derive(
+      popupList.map(p =>
+        bind(p, "revealChild")),
+      (...r) => r.reduce(
+        (a, b) => a || b)
+    ))}>
     <box
       orientation={Gtk.Orientation.VERTICAL}
       valign={Gtk.Align.END}>
-      <Popup
-        observable={bind(audio.defaultSpeaker, "volume")}
-        widget={Slider({
-          iconName: bind(audio.defaultSpeaker, "volumeIcon"),
-          binding: bind(brightness, "screen"),
-        })} />
-      <Popup
-        observable={bind(brightness, "screen")}
-        widget={Slider({
-          iconName: "display-brightness-symbolic",
-          binding: bind(brightness, "screen"),
-        })} />
+      {popupList}
     </box>
   </window>
