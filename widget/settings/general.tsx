2acdf5041e5c4e0bf368485219978c53d10f0297
caioasmuniz <caiomuniz888@gmail.com>
2025-05-17 18:00:56 -0300

    wraps general config in preferencesPage, some style changes

--- widget/settings/general.tsx
+++ widget/settings/general.tsx
@@ -4,36 +4,54 @@ import Adw from "gi://Adw?version=1";
 import { Config } from ".";
 
 export default (config: State<Config>) =>
-  <Adw.PreferencesGroup>
-    <Adw.ActionRow title={"System Theme"}>
-      <Adw.ToggleGroup _type="suffix">
-        <Adw.Toggle
-          iconName={"weather-clear-symbolic"}
-        />
-        <Adw.Toggle
-          iconName={"night-light-symbolic"}
-        />
-        <Adw.Toggle
-          iconName={"weather-clear-night-symbolic"}
-        />
-      </Adw.ToggleGroup>
-    </Adw.ActionRow>
+  <Adw.PreferencesPage>
+    <Adw.PreferencesGroup
+      title={"Appearance"}
+      description={"Set cosmetic options"}>
+      <Adw.ActionRow title={"System Theme"}>
+        <Adw.ToggleGroup _type="suffix"
+          cssClasses={["round"]}
+          valign={Gtk.Align.CENTER}>
+          <Adw.Toggle
+            label={"Light"}
+            iconName={"weather-clear-symbolic"}
+          />
+          <Adw.Toggle
+            label={"Auto"}
+            iconName={"night-light-symbolic"}
+          />
+          <Adw.Toggle
+            label={"Dark"}
+            iconName={"weather-clear-night-symbolic"}
+          />
+        </Adw.ToggleGroup>
+      </Adw.ActionRow>
 
-    <Adw.ActionRow title={"Bar Orientation"}>
-      <Adw.ToggleGroup _type="suffix"
-        $$active={self => config.set({
-          ...config.get(),
-          barOrientation: self.active as Gtk.Orientation
-        })}
-        active={bind(config).as(c =>
-          c.barOrientation as number ?? 0
-        )}>
-        <Adw.Toggle
-          iconName={"object-flip-horizontal-symbolic"}
-        />
-        <Adw.Toggle
-          iconName={"object-flip-vertical-symbolic"}
-        />
-      </Adw.ToggleGroup>
-    </Adw.ActionRow>
-  </Adw.PreferencesGroup> as Gtk.Widget
+      <Adw.ActionRow
+        title={"Bar Orientation"}
+        subtitle={bind(config).as(c =>
+          c.barOrientation! === Gtk.Orientation.HORIZONTAL ?
+          "Horizontal" : "Vertical")}>
+        <Adw.ToggleGroup
+          _type="suffix"
+          cssClasses={["round"]}
+          valign={Gtk.Align.CENTER}
+          $$active={self => config.set({
+            ...config.get(),
+            barOrientation: self.active as Gtk.Orientation
+          })}
+          active={bind(config).as(c =>
+            c.barOrientation as number ?? 0
+          )}>
+          <Adw.Toggle
+            label={"Horizontal"}
+            iconName={"object-flip-horizontal-symbolic"}
+          />
+          <Adw.Toggle
+            label={"Vertical"}
+            iconName={"object-flip-vertical-symbolic"}
+          />
+        </Adw.ToggleGroup>
+      </Adw.ActionRow>
+    </Adw.PreferencesGroup>
+  </Adw.PreferencesPage> as Gtk.Widget
