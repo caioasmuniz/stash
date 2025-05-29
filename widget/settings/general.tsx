c19ac1c83eba5f52bf023ad5544e318657a2ed21
caioasmuniz <caiomuniz888@gmail.com>
2025-05-29 13:55:06 -0300

    simplify page layout

--- widget/settings/general.tsx
+++ widget/settings/general.tsx
@@ -7,81 +7,76 @@ const settings = Settings.get_default()
 const { TOP, LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor
 
 export default () =>
-  <Adw.ViewStackPage
-    title="General"
-    iconName="preferences-desktop-appearance-symbolic"
-    child={
-      <Adw.PreferencesGroup
-        title={"Appearance"}
-        description={"Set cosmetic options"}>
-        <Adw.ActionRow title={"System Theme"}>
-          <Adw.ToggleGroup _type="suffix"
-            cssClasses={["round"]}
-            valign={Gtk.Align.CENTER}>
-            <Adw.Toggle
-              label={"Light"}
-              iconName={"weather-clear-symbolic"}
-            />
-            <Adw.Toggle
-              label={"Auto"}
-              iconName={"night-light-symbolic"}
-            />
-            <Adw.Toggle
-              label={"Dark"}
-              iconName={"weather-clear-night-symbolic"}
-            />
-          </Adw.ToggleGroup>
-        </Adw.ActionRow>
+    <Adw.PreferencesGroup
+      title={"Appearance"}
+      description={"Set cosmetic options"}>
+      <Adw.ActionRow title={"System Theme"}>
+        <Adw.ToggleGroup _type="suffix"
+          cssClasses={["round"]}
+          valign={Gtk.Align.CENTER}>
+          <Adw.Toggle
+            label={"Light"}
+            iconName={"weather-clear-symbolic"}
+          />
+          <Adw.Toggle
+            label={"Auto"}
+            iconName={"night-light-symbolic"}
+          />
+          <Adw.Toggle
+            label={"Dark"}
+            iconName={"weather-clear-night-symbolic"}
+          />
+        </Adw.ToggleGroup>
+      </Adw.ActionRow>
 
-        <Adw.ActionRow
-          title={"Bar Orientation"}
-          subtitle={bind(settings.bar, "position")
-            .as(position => {
-              switch (position) {
-                case TOP:
-                  return "Top";
-                case LEFT:
-                  return "Left";
-                case RIGHT:
-                  return "Right";
-                case BOTTOM:
-                  return "Bottom";
-                default:
-                  return "";
-              }
-            })
-          }>
-          <Adw.ToggleGroup
-            _type="suffix"
-            cssClasses={["round"]}
-            valign={Gtk.Align.CENTER}
-            $$activeName={self => settings.bar.position =
-              Number(self.activeName) as Astal.WindowAnchor
+      <Adw.ActionRow
+        title={"Bar Orientation"}
+        subtitle={bind(settings.bar, "position")
+          .as(position => {
+            switch (position) {
+              case TOP:
+                return "Top";
+              case LEFT:
+                return "Left";
+              case RIGHT:
+                return "Right";
+              case BOTTOM:
+                return "Bottom";
+              default:
+                return "";
             }
-            activeName={bind(settings.bar, "position")
-              .as(position => position.toString() ?? "")
-            }>
-            <Adw.Toggle
-              name={TOP.toString()}
-              label={"Top"}
-              iconName={"orientation-landscape-symbolic"}
-            />
-            <Adw.Toggle
-              name={LEFT.toString()}
-              label={"Left"}
-              iconName={"orientation-portrait-inverse-symbolic"}
-            />
-            <Adw.Toggle
-              name={RIGHT.toString()}
-              label={"Right"}
-              iconName={"orientation-portrait-right-symbolic"}
-            />
-            <Adw.Toggle
-              name={BOTTOM.toString()}
-              label={"Bottom"}
-              iconName={"orientation-landscape-inverse-symbolic"}
-            />
-          </Adw.ToggleGroup>
-        </Adw.ActionRow>
-      </Adw.PreferencesGroup> as Gtk.Widget
-    } /> 
+          })
+        }>
+        <Adw.ToggleGroup
+          _type="suffix"
+          cssClasses={["round"]}
+          valign={Gtk.Align.CENTER}
+          $$activeName={self => settings.bar.position =
+            Number(self.activeName) as Astal.WindowAnchor
+          }
+          activeName={bind(settings.bar, "position")
+            .as(position => position.toString() ?? "")
+          }>
+          <Adw.Toggle
+            name={TOP.toString()}
+            label={"Top"}
+            iconName={"orientation-landscape-symbolic"}
+          />
+          <Adw.Toggle
+            name={LEFT.toString()}
+            label={"Left"}
+            iconName={"orientation-portrait-inverse-symbolic"}
+          />
+          <Adw.Toggle
+            name={RIGHT.toString()}
+            label={"Right"}
+            iconName={"orientation-portrait-right-symbolic"}
+          />
+          <Adw.Toggle
+            name={BOTTOM.toString()}
+            label={"Bottom"}
+            iconName={"orientation-landscape-inverse-symbolic"}
+          />
+        </Adw.ToggleGroup>
+      </Adw.ActionRow>
+    </Adw.PreferencesGroup>
--- widget/settings/index.tsx
+++ widget/settings/index.tsx
@@ -1,29 +1,9 @@
 import { Gtk } from "ags/gtk4";
 import app from "ags/gtk4/app";
 import Adw from "gi://Adw?version=1";
-import general from "./general";
 import General from "./general";
 
 export default () => {
-  const pages = [{
-    title: "General",
-    iconName: "preferences-desktop-appearance-symbolic",
-    widget: general()
-  }, {
-    title: "Desktop",
-    iconName: "preferences-desktop-display-symbolic",
-    widget: <label label={"view 2"} /> as Gtk.Widget
-  }];
-
-  const stack = <Adw.ViewStack
-    enableTransitions>
-    <General />
-    <Adw.ViewStackPage
-      title={"Desktop"}
-      iconName={"preferences-desktop-display-symbolic"}
-      child={<label label={"view 2"} /> as Gtk.Widget} />
-  </Adw.ViewStack>
-
   return <Adw.Window
     hideOnClose
     name={"settings"}
@@ -31,11 +11,9 @@ export default () => {
     cssClasses={["background"]}
     title={"Stash Settings"}>
     <box orientation={Gtk.Orientation.VERTICAL}>
-      <Adw.InlineViewSwitcher
-        cssClasses={["round"]}
-        displayMode={Adw.InlineViewSwitcherDisplayMode.BOTH}
-        stack={stack as Adw.ViewStack} />
-      {stack}
+      <Adw.PreferencesPage>
+        <General />
+      </Adw.PreferencesPage>
     </box>
   </Adw.Window >
 }
