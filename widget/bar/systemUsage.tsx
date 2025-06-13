b2f6f12e58b109487d766bf1f3fb08d7e4d39e09
caioasmuniz <caiomuniz888@gmail.com>
2025-06-13 17:13:41 -0300

    small systemUsage modifications

--- widget/bar/systemUsage.tsx
+++ widget/bar/systemUsage.tsx
@@ -5,81 +5,80 @@ import { Gdk, Gtk } from "ags/gtk4";
 import GTop from "gi://GTop";
 import Settings from "../../lib/settings";
 
-const settings = Settings.get_default()
+export default ({ vertical }: { vertical: boolean }) => {
+  const settings = Settings.get_default()
 
-const [lastCpuTop, setLastCpuTop] = createState(new GTop.glibtop_cpu())
-const INTERVAL = 1000;
+  const [lastCpuTop, setLastCpuTop] = createState(new GTop.glibtop_cpu())
+  const INTERVAL = 1000;
 
-const cpu = createPoll(0, INTERVAL, () => {
-  const cpuTop = new GTop.glibtop_cpu()
-  GTop.glibtop_get_cpu(cpuTop);
-  const total = cpuTop.total - lastCpuTop.get().total;
-  const user = cpuTop.user - lastCpuTop.get().user;
-  const sys = cpuTop.sys - lastCpuTop.get().sys;
-  const nice = cpuTop.nice - lastCpuTop.get().nice;
-  setLastCpuTop(cpuTop)
-  return (user + sys + nice) / total;
-})
+  const cpu = createPoll(0, INTERVAL, () => {
+    const cpuTop = new GTop.glibtop_cpu()
+    GTop.glibtop_get_cpu(cpuTop);
+    const total = cpuTop.total - lastCpuTop.get().total;
+    const user = cpuTop.user - lastCpuTop.get().user;
+    const sys = cpuTop.sys - lastCpuTop.get().sys;
+    const nice = cpuTop.nice - lastCpuTop.get().nice;
+    setLastCpuTop(cpuTop)
+    return (user + sys + nice) / total;
+  })
 
-const memory = createPoll(0, INTERVAL, () => {
-  const memTop = new GTop.glibtop_mem()
-  GTop.glibtop_get_mem(memTop);
-  return memTop.user / memTop.total;
-})
+  const memory = createPoll(0, INTERVAL, () => {
+    const memTop = new GTop.glibtop_mem()
+    GTop.glibtop_get_mem(memTop);
+    return memTop.user / memTop.total;
+  })
 
-const disk = createPoll(0, INTERVAL, () => {
-  const diskTop = new GTop.glibtop_fsusage()
-  GTop.glibtop_get_fsusage(diskTop, "/");
-  return diskTop.bavail / diskTop.bfree;
-})
+  const disk = createPoll(0, INTERVAL, () => {
+    const diskTop = new GTop.glibtop_fsusage()
+    GTop.glibtop_get_fsusage(diskTop, "/");
+    return diskTop.bavail / diskTop.bfree;
+  })
 
-const temp = createPoll(0, INTERVAL, () => {
-  if (settings.bar.tempPath)
-    return parseInt(
-      exec(`cat ${settings.bar.tempPath}`)
-    ) / 100000
-  else
-    return 0
-})
-// out => parseInt(out) / 100000)
+  const temp = createPoll(0, INTERVAL, () => {
+    if (settings.bar.tempPath)
+      return parseInt(
+        exec(`cat ${settings.bar.tempPath}`)
+      ) / 100000
+    else
+      return 0
+  })
 
-const Indicator = ({ value, label, unit, vertical, visible = true }:
-  {
-    value: Accessor<number>,
-    label: string,
-    unit: string,
-    vertical: boolean
-    visible?: Accessor<boolean> | boolean
-  }) => <levelbar
-    visible={visible}
-    orientation={vertical ?
-      Gtk.Orientation.VERTICAL :
-      Gtk.Orientation.HORIZONTAL}
-    inverted={vertical}
-    value={value}
-    widthRequest={vertical ? -1 : 50}
-    heightRequest={vertical ? 50 : -1}>
-    <box
-      valign={Gtk.Align.CENTER}
-      halign={Gtk.Align.CENTER}
-      spacing={2}
+  const Indicator = ({ value, label, unit, vertical, visible = true }:
+    {
+      value: Accessor<number>,
+      label: string,
+      unit: string,
+      vertical: boolean
+      visible?: Accessor<boolean> | boolean
+    }) => <Gtk.LevelBar
+      visible={visible}
       orientation={vertical ?
         Gtk.Orientation.VERTICAL :
-        Gtk.Orientation.HORIZONTAL}>
-      <label
-        label={label}
-        cssClasses={["title"]} />
-      <label
-        cssClasses={["body"]}
-        label={value(v =>
-          (v * 100)
-            .toFixed(0)
-            .concat(unit))} />
-    </box>
-  </levelbar >
+        Gtk.Orientation.HORIZONTAL}
+      inverted={vertical}
+      value={value}
+      widthRequest={vertical ? -1 : 50}
+      heightRequest={vertical ? 50 : -1}>
+      <box
+        valign={Gtk.Align.CENTER}
+        halign={Gtk.Align.CENTER}
+        spacing={2}
+        orientation={vertical ?
+          Gtk.Orientation.VERTICAL :
+          Gtk.Orientation.HORIZONTAL}>
+        <label
+          label={label}
+          cssClasses={["title"]} />
+        <label
+          cssClasses={["body"]}
+          label={value(v =>
+            (v * 100)
+              .toFixed(0)
+              .concat(unit))} />
+      </box>
+    </Gtk.LevelBar >
 
-export default ({ vertical }: { vertical: boolean }) =>
-  <button
+  return <button
     cursor={Gdk.Cursor.new_from_name("pointer", null)}
     $clicked={() =>
       settings.bar.systemMonitor ?
@@ -108,10 +107,12 @@ export default ({ vertical }: { vertical: boolean }) =>
         value={temp}
         label="TEMP"
         unit="°C" />
-      {/* <Indicator
+      <Indicator
         vertical={vertical}
-        value={bind(disk)}
+        visible={false}
+        value={disk}
         label="DISK"
-        unit="%" /> */}
+        unit="%" />
     </box>
   </button >
+}
