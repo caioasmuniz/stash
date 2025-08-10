2a732478a1a5d7fd9fc06fbf92923c23c2517e2c
caioasmuniz <caiomuniz888@gmail.com>
2025-08-10 20:41:38 -0300

    fix media widget

--- widget/quicksettings/media.tsx
+++ widget/quicksettings/media.tsx
@@ -13,29 +13,89 @@ function lengthStr(length: number) {
   return `${min}:${sec0}${sec}`;
 }
 
-const PlaybackButtons = ({ player }: { player: Mpris.Player }) => <box>
-  <button
-    onClicked={() => player.previous()}
-    visible={player.canGoPrevious}>
-    <image iconName={"media-skip-backwiconNameard-symbolic"} />
-  </button>
-
-  <button
-    onClicked={() =>
-      player.playbackStatus === Mpris.PlaybackStatus.PAUSED
-        ? player.play() : player.pause()}>
+const PlayerApp = ({ player }: { player: Mpris.Player }) =>
+  <box
+    halign={Gtk.Align.START}
+    spacing={4}>
     <image
-      iconName={createBinding(player, "playbackStatus")
-        (s => s === Mpris.PlaybackStatus.PLAYING
-          ? "media-playback-pause-symbolic"
-          : "media-playback-start-symbolic")} />
-  </button>
-  <button
-    onClicked={() => player.next()}
-    visible={player.canGoNext}>
-    <image iconName={"media-skip-forward-symbolic"} />
-  </button>
-</box>
+      cssClasses={["icon"]}
+      hexpand
+      tooltipText={createBinding(player, "identity")
+        .as(id => id || "")}
+      iconName={createBinding(player, "entry")
+        .as(entry => apps.exact_query(entry)[0]!.iconName)} />
+    <label label={createBinding(player, "identity")
+      .as(id => id || "")} />
+  </box>
+
+const CoverArt = ({ player }: { player: Mpris.Player }) =>
+  <image
+    file={createBinding(player, "coverArt")}
+    cssClasses={["thumbnail"]}
+    hexpand
+  />
+
+const TitleArtist = ({ player }: { player: Mpris.Player }) =>
+  <box
+    orientation={Gtk.Orientation.VERTICAL}
+    hexpand>
+    <label
+      wrap
+      maxWidthChars={10}
+      cssClasses={["heading"]}
+      label={createBinding(player, "title")}
+    />
+    <label
+      cssClasses={["artist"]}
+      label={createBinding(player, "artist")} />
+  </box>
+
+const PlaybackButtons = ({ player }: { player: Mpris.Player }) =>
+  <box>
+    <button
+      onClicked={() => player.previous()}
+      visible={player.canGoPrevious}>
+      <image iconName={"media-skip-backwiconNameard-symbolic"} />
+    </button>
+
+    <button
+      onClicked={() =>
+        player.playbackStatus === Mpris.PlaybackStatus.PAUSED
+          ? player.play() : player.pause()}>
+      <image
+        iconName={createBinding(player, "playbackStatus")
+          (s => s === Mpris.PlaybackStatus.PLAYING
+            ? "media-playback-pause-symbolic"
+            : "media-playback-start-symbolic")} />
+    </button>
+    <button
+      onClicked={() => player.next()}
+      visible={player.canGoNext}>
+      <image iconName={"media-skip-forward-symbolic"} />
+    </button>
+  </box>
+
+const PlaybackStatus = ({ player }: { player: Mpris.Player }) =>
+  <box orientation={Gtk.Orientation.VERTICAL}>
+    <slider
+      cssClasses={["position"]}
+      drawValue={false}
+      onNotifyValue={({ value }) =>
+        player.position = value}
+      min={0}
+      max={createBinding(player, "length")}
+      visible={createBinding(player, "canSeek")}
+      value={createBinding(player, "position")} />
+    <centerbox>
+      <label $type="start"
+        label={createBinding(player, "position")
+          .as(lengthStr)} />
+      <PlaybackButtons $type="center" player={player} />
+      <label $type="end"
+        label={createBinding(player, "length")
+          .as(lengthStr)} />
+    </centerbox>
+  </box>
 
 export default () => <box
   orientation={Gtk.Orientation.VERTICAL}
@@ -47,42 +107,12 @@ export default () => <box
         cssClasses={["media"]}
         orientation={Gtk.Orientation.VERTICAL}
         hexpand>
+        <PlayerApp player={player} />
         <box>
-          <image
-            file={player.coverArt}
-            cssClasses={["thumbnail"]}
-            hexpand
-          />
-          <label
-            wrap
-            maxWidthChars={10}
-            cssClasses={["heading"]}
-            label={createBinding(player, "title")} />
-          <image
-            cssClasses={["icon"]}
-            hexpand
-            tooltipText={createBinding(player, "identity")
-              (id => id || "")}
-            iconName={createBinding(player, "entry")
-              (entry => apps.exact_query(entry)[0]!.iconName)} />
+          <CoverArt player={player} />
+          <TitleArtist player={player} />
         </box>
-        <label
-          cssClasses={["artist"]}
-          label={createBinding(player, "artist")} />
-        <slider
-          cssClasses={["position"]}
-          drawValue={false}
-          // onDragged={({ value }) => player.position = value}
-          min={0}
-          max={createBinding(player, "length")}
-          visible={createBinding(player, "canSeek")}
-          value={createBinding(player, "position")} />
-        <centerbox>
-          <label
-            label={createBinding(player, "position")(lengthStr)} />
-          <PlaybackButtons player={player} />
-          <label label={createBinding(player, "length")(lengthStr)} />
-        </centerbox>
+        <PlaybackStatus player={player} />
       </box>
     }
   </For>
