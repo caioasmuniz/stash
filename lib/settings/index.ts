6d0d23353e213d6c2c7a4223fde2c115cf6bef2b
caioasmuniz <caiomuniz888@gmail.com>
2025-08-12 18:54:27 -0300

    remove unused file, update devshell envvar, add context-related code to settings

--- lib/settings/index.ts
+++ lib/settings/index.ts
@@ -1,16 +1,27 @@
-import { createSettings } from "gnim";
+import { createContext, createSettings } from "gnim";
 import Gio from "gi://Gio?version=2.0";
 
 export default () => {
   const s = createSettings(new Gio.Settings({
     schema_id: "stash.bar"
-  }), {
-    "position": "i",
-    "temp-path": "s",
-    "system-monitor": "s"
-  })
+  }), schema)
   return {
     bar: s
   }
 }
 
+export const schema = Object.freeze({
+  "position": "i",
+  "temp-path": "s",
+  "system-monitor": "s"
+})
+
+type Settings = ReturnType<typeof createSettings<typeof schema>>
+
+export const SettingsContext = createContext<Settings | null>(null)
+
+export function useSettings() {
+  const settings = SettingsContext.use()
+  if (!settings) throw Error("settings not in scope")
+  return settings
+}
