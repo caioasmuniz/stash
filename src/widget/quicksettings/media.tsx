8b5aaf6265d5a8bfab99eb6c8d6b9de4e99949ec
caioasmuniz <caiomuniz888@gmail.com>
2025-08-20 13:11:16 -0300

    removed intrinsic elements from media

--- src/widget/quicksettings/media.tsx
+++ src/widget/quicksettings/media.tsx
@@ -1,3 +1,4 @@
+import Astal from "gi://Astal?version=4.0";
 import Mpris from "gi://AstalMpris";
 import Apps from "gi://AstalApps"
 import Gtk from "gi://Gtk?version=4.0";
@@ -14,70 +15,70 @@ function lengthStr(length: number) {
 }
 
 const PlayerApp = ({ player }: { player: Mpris.Player }) =>
-  <box
+  <Gtk.Box
     halign={Gtk.Align.START}
     spacing={4}>
-    <image
+    <Gtk.Image
       cssClasses={["icon"]}
       hexpand
       tooltipText={createBinding(player, "identity")
         .as(id => id || "")}
       iconName={createBinding(player, "entry")
         .as(entry => apps.exact_query(entry)[0]!.iconName)} />
-    <label label={createBinding(player, "identity")
+    <Gtk.Label label={createBinding(player, "identity")
       .as(id => id || "")} />
-  </box>
+  </Gtk.Box>
 
 const CoverArt = ({ player }: { player: Mpris.Player }) =>
-  <image
+  <Gtk.Image
     file={createBinding(player, "coverArt")}
     cssClasses={["thumbnail"]}
     hexpand
   />
 
 const TitleArtist = ({ player }: { player: Mpris.Player }) =>
-  <box
+  <Gtk.Box
     orientation={Gtk.Orientation.VERTICAL}
     hexpand>
-    <label
+    <Gtk.Label
       wrap
       maxWidthChars={10}
       cssClasses={["heading"]}
       label={createBinding(player, "title")}
     />
-    <label
+    <Gtk.Label
       cssClasses={["artist"]}
       label={createBinding(player, "artist")} />
-  </box>
+  </Gtk.Box>
 
 const PlaybackButtons = ({ player }: { player: Mpris.Player }) =>
-  <box>
-    <button
+  <Gtk.Box>
+    <Gtk.Button
       onClicked={() => player.previous()}
       visible={player.canGoPrevious}>
-      <image iconName={"media-skip-backwiconNameard-symbolic"} />
-    </button>
+      <Gtk.Image iconName={"media-skip-backwiconNameard-symbolic"} />
+    </Gtk.Button>
 
-    <button
+    <Gtk.Button
       onClicked={() =>
         player.playbackStatus === Mpris.PlaybackStatus.PAUSED
           ? player.play() : player.pause()}>
-      <image
+      <Gtk.Image
         iconName={createBinding(player, "playbackStatus")
-          (s => s === Mpris.PlaybackStatus.PLAYING
+          .as(s => s === Mpris.PlaybackStatus.PLAYING
             ? "media-playback-pause-symbolic"
             : "media-playback-start-symbolic")} />
-    </button>
-    <button
+    </Gtk.Button>
+    <Gtk.Button
       onClicked={() => player.next()}
       visible={player.canGoNext}>
-      <image iconName={"media-skip-forward-symbolic"} />
-    </button>
-  </box>
+      <Gtk.Image iconName={"media-skip-forward-symbolic"} />
+    </Gtk.Button>
+  </Gtk.Box>
 
 const PlaybackStatus = ({ player }: { player: Mpris.Player }) =>
-  <box orientation={Gtk.Orientation.VERTICAL}>
-    <slider
+  <Gtk.Box orientation={Gtk.Orientation.VERTICAL}>
+    <Astal.Slider
       cssClasses={["position"]}
       drawValue={false}
       onNotifyValue={({ value }) =>
@@ -86,16 +87,16 @@ const PlaybackStatus = ({ player }: { player: Mpris.Player }) =>
       max={createBinding(player, "length")}
       visible={createBinding(player, "canSeek")}
       value={createBinding(player, "position")} />
-    <centerbox>
-      <label $type="start"
+    <Gtk.CenterBox>
+      <Gtk.Label $type="start"
         label={createBinding(player, "position")
           .as(lengthStr)} />
       <PlaybackButtons $type="center" player={player} />
-      <label $type="end"
+      <Gtk.Label $type="end"
         label={createBinding(player, "length")
           .as(lengthStr)} />
-    </centerbox>
-  </box>
+    </Gtk.CenterBox>
+  </Gtk.Box>
 
 export default () => <Gtk.Box
   orientation={Gtk.Orientation.VERTICAL}
